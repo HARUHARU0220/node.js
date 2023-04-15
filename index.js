b1272e713d3970dcbab9b9875a67dc7128afeb62
HARU <202310101221@hycu.ac.kr>
2023-04-15 12:32:28 +0900

    request response test

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import express from "express"
 const app = express()
 
+// request, response test
+app.get("/test", (req, res) => {
+    res.json({
+        msg: "data get all"
+    })
+})
 
 
 
 
 
-
-const port = 2020
+const port = 8080
 app.listen(port, console.log("server start"))
