f260ed12515bf921d1894d954827e12baa550a9f
HARU <202310101221@hycu.ac.kr>
2023-04-29 11:15:22 +0900

    add mongoose

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ import express from "express"
 import bodyParser from "body-parser";
 import morgan from "morgan"
 import dotenv from "dotenv"
+import connectDB from "./config/database.js"
+
 const app = express()
+
 
 dotenv.config()
 
+
+// database 연결
+connectDB()
 
 // request, response test
 // app.get("/test", (req, res) => {
