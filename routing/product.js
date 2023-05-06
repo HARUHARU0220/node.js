fff5f573b91b2db43a178c8afc55818a0f1597f5
HARU <202310101221@hycu.ac.kr>
2023-05-06 12:23:38 +0900

    product get all / detail

--- routing/product.js
+++ routing/product.js
@@ -2,6 +2,28 @@ import express from "express";
 import productModel from "../models/product.js";
 const routing = express.Router()
 
+
+// Product 전체 데이터를 가져오는 API
+routing.get("/", async (req,res) => {
+    const products = await productModel.find()
+    res.json({
+        msg: "Product get all",
+        products: products
+    })
+})
+
+// 상세 프로덕트를 가져오는 API
+
+routing.get("/:productid", async (req, res) => {
+    const product = await productModel.findById(req.params.productid)
+    res.json({
+        msg: "Product detail get",
+        product : product
+    })
+})
+
+
+
 routing.post("/",async (req,res)=>{
     // name, price, description, stock
     const newProduct = new productModel ({
